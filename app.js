a1ffb7a7e128a12eaeda701546d1bf541e4b2308
caioagralemos <caioagralemos@live.com>
2023-05-28 12:34:44 -0300

    /en

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const path = require("path")
 app.set("view engine", "ejs") // request e ja define ejs como nossa engine
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.get('/en', (req,res) => {
+    res.render('home-en.ejs')
+})
+
 app.get("*", (req, res) => {
     res.render("home.ejs")
 })
